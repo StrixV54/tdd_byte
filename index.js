42b3691aa8c19d51c6259a420cfc02820b37eb4c
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2025-08-04 01:05:33 +0530

    feat: adds error throwing for negative numbers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function add(numbers) {
     if (!numbers) return 0;
+
     // storing delimiters in array for scalability
     let delimiters = [',', '\n'];
     // checking for custom delimiters with pattern : "//[delimiter]\n[numbers…]"
@@ -8,9 +9,20 @@ function add(numbers) {
         numbers = numbers.slice(String(numbers).indexOf('\n'));
     }
     let separatedNum = String(numbers).split(new RegExp(delimiters.join('|')));
+
+    // checking and storing for negative numbers
+    let negativeNumbers = [];
+    for (let num of separatedNum) {
+        if (+num < 0) negativeNumbers.push(num);
+    }
+    if (negativeNumbers.length > 0)
+        throw new Error("Negative number not allowed " + negativeNumbers?.join(', '));
+
+    // adding numbers after separating
     return separatedNum?.reduce((acc, curr) => {
         return acc + +curr
     }, 0)
+
 }
 
 module.exports = { add };
